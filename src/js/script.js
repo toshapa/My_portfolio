734ebf3e8b9f7c32620b18fb786c73432e4a69a6
Pechtnov <tonichok@gmail.com>
2022-10-22 17:20:36 +0300

    added script for hide  last height line on block os my skill

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,8 @@ let closeElem = document.querySelector('.menu__close_btn');
 let menuOverlay = document.querySelector('.menu__overlay')
 let menu = document.querySelector('.menu');
 
+let lastline = document.querySelectorAll('.aboutMe__skill_heightLine');
+
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
     menuOverlay.classList.add('active');
@@ -15,3 +17,9 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
     menuOverlay.classList.remove('active');
 });
+
+let line = lastline[lastline.length - 1];
+line.style.width = '0px';
+
+
+
