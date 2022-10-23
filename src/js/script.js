2ff968d46335fac45b5966425bf959005dbd38cb
Pechtnov <tonichok@gmail.com>
2022-10-23 21:41:40 +0300

    cleared js

--- src/js/script.js
+++ src/js/script.js
@@ -3,15 +3,6 @@ let closeElem = document.querySelector('.menu__close_btn');
 let menuOverlay = document.querySelector('.menu__overlay')
 let menu = document.querySelector('.menu');
 
-let lastline = document.querySelectorAll('#heightLine');
-// console.log(lastline);
-
-
-
-let ulFirstBlock = document.querySelectorAll('ul')
-console.log(ulFirstBlock)
-console.log(ulFirstBlock[1].children[4].children[0].childNodes[1])
-
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
     menuOverlay.classList.add('active');
@@ -24,8 +15,3 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
     menuOverlay.classList.remove('active');
 });
-
-
-// ulFirstBlock[1].children[4].children[0].childNodes[1].style.width = '0px';
-
-
