6ee0c4e73d64d30929a586df50d3c9fbf5d4eb3f
Pechtnov <tonichok@gmail.com>
2022-10-27 17:18:21 +0300

    added a section with portfolio, then added a script for aside teg

--- src/js/script.js
+++ src/js/script.js
@@ -2,16 +2,23 @@ let hamburger = document.querySelector('.hamburger')
 let closeElem = document.querySelector('.menu__close_btn');
 let menuOverlay = document.querySelector('.menu__overlay')
 let menu = document.querySelector('.menu');
+let menuLink = [];
+menuLink = document.querySelectorAll('.menu__link')
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
     menuOverlay.classList.add('active');
 
 });
-
-
-closeElem.addEventListener('click', () => {
-
+function hide () {
     menu.classList.remove('active');
     menuOverlay.classList.remove('active');
-});
+    console.log('blia')
+}
+
+closeElem.addEventListener('click', hide);
+menuOverlay.addEventListener('click', hide);
+
+for (i = 0; i < menuLink.length; i++) {
+    menuLink[i].addEventListener('click', hide)
+}
